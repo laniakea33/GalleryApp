8e26b347b433707159eed26a6e2ccca1c0adf4fd
laniakea33 <laniakea33@github.com>
2024-11-24 16:46:02 +0900

    KeyResultNotifier keyIndexMap 네이밍 변경

--- app/src/main/java/com/dh/galleryapp/feature/list/keystatusmap/KeyResultNotifier.kt
+++ app/src/main/java/com/dh/galleryapp/feature/list/keystatusmap/KeyResultNotifier.kt
@@ -2,46 +2,50 @@ package com.dh.galleryapp.feature.list.keystatusmap
 
 import com.dh.galleryapp.feature.list.model.ImageResult
 
-class KeyResultNotifier  {
+class KeyResultNotifier {
 
     //  이미지 key별 index. 같은 key를 여러 index에서 요청할 경우를 위함.
     //  observe할 때 add되고 제거되지 않는다.
     //  observe할 때 isActive = true, dispose할 때 isActive = false
-    private val observers = KeyIndexMap()
+    private val keyIndexMap = KeyIndexMap()
 
     //  이미지 key별 응답 상태. 같은 key를 여러 index에서 요청할 경우를 위함.
     //  Wait, Loading, Success, Failure 4개만 가진다.
     //  해당 key가 처음 observe될 때 add, key를 observing하는 index가 모두 dispose되면 remove
-    private val imageResults = mutableMapOf<String, ImageResult>()
+    private val keyResultMap = mutableMapOf<String, ImageResult>()
 
     fun observe(key: String, index: Int) {
-        observers.putOrUpdate(key, index, true)
+        keyIndexMap.putOrUpdate(key, index, true)
 
-        if (imageResults[key] == null) {
-            imageResults[key] = ImageResult.Waiting
+        if (keyResultMap[key] == null) {
+            keyResultMap[key] = ImageResult.Waiting
         }
     }
 
     fun getImageResult(key: String): ImageResult? {
-        return imageResults[key]
+        return keyResultMap[key]
     }
 
     fun dispose(key: String, index: Int) {
-        observers.putOrUpdate(key, index, false)
+        keyIndexMap.putOrUpdate(key, index, false)
         if (hasNoObserver(key)) {
-            imageResults.remove(key)
+            keyResultMap.remove(key)
         }
     }
 
     fun hasNoObserver(key: String): Boolean {
-        return observers.getActiveCount(key) == 0
+        return keyIndexMap.getActiveCount(key) == 0
     }
 
-    fun updateImageResult(key: String, imageResult: ImageResult, notifier: (observerIndex: Int) -> Unit) {
-        if (imageResults[key] != null) {
-            imageResults[key] = imageResult
+    fun updateImageResult(
+        key: String,
+        imageResult: ImageResult,
+        notifier: (observerIndex: Int) -> Unit,
+    ) {
+        if (keyResultMap[key] != null) {
+            keyResultMap[key] = imageResult
         }
 
-        observers.forEachActive(key, notifier)
+        keyIndexMap.forEachActive(key, notifier)
     }
 }
